c24c663542ba6be1a918263c1823584561242c15
oleg166 <demeoleg@mail.ru>
2021-11-24 22:37:43 +0300

    Выполнено

--- Lesson_1/one.py
+++ Lesson_1/one.py
@@ -10,6 +10,17 @@ req = requests.get(f'{service}/users/{user}/repos')
 for i in req.json():
     print(i['name'])
 
+"""
+17_kurs
+framepaper
+HTML_CSS_29.05.2020
+Intergalactic_Entertainment
+kellolo-js-2-21-1209
+Preparing
+Python_Algos
+Python_Basics_17.12.2019
+"""
+
 # сохранение JSON-вывода в файле *.json
 with open('data1.json', 'w') as f:
     json.dump(req.json(), f)
