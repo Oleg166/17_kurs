f911f03d003844b0aab4e299d6570a5be21b2c1d
oleg166 <demeoleg@mail.ru>
2021-12-15 23:07:56 +0300

    Выполнил задание к 6-ой лекции

--- Lesson_6/labirintparser/items.py
+++ Lesson_6/labirintparser/items.py
@@ -9,4 +9,10 @@ import scrapy
 class LabirintparserItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    link = scrapy.Field()
+    name = scrapy.Field()
+    _id = scrapy.Field()
+    authors = scrapy.Field()
+    price = scrapy.Field()
+    # price_discount = scrapy.Field()
+    rating = scrapy.Field()
